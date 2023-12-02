b66f503b1b11a968b6096e783233cca062c6fcae
Justin113D <Justin113D@gmail.com>
2023-12-02 12:40:54 +0100

    Fix: Region marker not working correctly

--- src/SA3D.Common/RegionMarker.cs
+++ src/SA3D.Common/RegionMarker.cs
@@ -10,86 +10,82 @@ namespace SA3D.Common
 	/// <typeparam name="T">Marker Datatype</typeparam>
 	public class RegionMarker<T> where T : unmanaged, IEquatable<T>
 	{
-		private readonly SortedDictionary<uint, T> _regions = new() { { 0, default } };
-		private readonly HashSet<uint> _toRemoveBuffer = new();
+		private readonly SortedDictionary<uint, T> _regions;
+
+		/// <summary>
+		/// Creates a new region marker with a default region value.
+		/// </summary>
+		/// <param name="defaultValue">Default value for all unmarked regions</param>
+		public RegionMarker(T defaultValue)
+		{
+			_regions = new() { { 0, defaultValue } };
+		}
+
+		/// <summary>
+		/// Creates a new region marker.
+		/// </summary>
+		public RegionMarker() : this(default) { }
 
 		/// <summary>
 		/// Marks a region with a value.
 		/// </summary>
-		/// <param name="from">Start of the region.</param>
-		/// <param name="to">End of the region.</param>
+		/// <param name="from">Start of the region (inclusive).</param>
+		/// <param name="to">End of the region (exclusive).</param>
 		/// <param name="value">Value to mark the region as.</param>
 		public void MarkRegion(uint from, uint to, T value)
 		{
-			T before = value;
-			T after = default;
-			uint nextIndex = 0;
-			T next = default;
+			T endValue = _regions.Last(x => x.Key < to).Value;
 
-			foreach(KeyValuePair<uint, T> item in _regions)
+			foreach(uint marker in _regions.Keys.ToArray())
 			{
-				if(item.Key < from)
+				if(marker < from)
 				{
-					before = item.Value;
+					continue;
 				}
-				else if(item.Key < to)
+
+				if(marker >= to)
 				{
-					_toRemoveBuffer.Add(item.Key);
-					after = item.Value;
-				}
-				else
-				{
-					next = item.Value;
-					nextIndex = item.Key;
 					break;
 				}
+
+				_regions.Remove(marker);
 			}
 
-			foreach(uint key in _toRemoveBuffer)
-			{
-				_regions.Remove(key);
-			}
-
-			if(from == 0 || !before.Equals(value))
+			if(from == 0 || !_regions.Last(x => x.Key < from).Value.Equals(value))
 			{
 				_regions.Add(from, value);
 			}
 
-			if(_regions.TryGetValue(to, out T currentAfter))
+			if(_regions.ContainsKey(to))
 			{
-				if(currentAfter.Equals(value))
+				if(_regions[to].Equals(value))
 				{
 					_regions.Remove(to);
 				}
 			}
-			else
+			else if(!value.Equals(endValue))
 			{
-				T newEndValue = value;
-				if(!after.Equals(value))
-				{
-					newEndValue = after;
-					_regions.Add(to, after);
-				}
-
-				if(nextIndex != 0 && newEndValue.Equals(next))
-				{
-					_regions.Remove(nextIndex);
-				}
+				_regions.Add(to, endValue);
 			}
-
-			_toRemoveBuffer.Clear();
 		}
 
 		/// <summary>
 		/// Used to check whether a region contains a specific value.
 		/// </summary>
-		/// <param name="from">Start of region to check.</param>
-		/// <param name="to">End of region to check.</param>
-		/// <param name="value">Value to check.</param>
+		/// <param name="from">Start of region to check (inclusive).</param>
+		/// <param name="to">End of region to check (exclusive).</param>
+		/// <param name="value">Value to check for.</param>
 		/// <returns></returns>
 		public bool HasValue(uint from, uint to, T value)
 		{
-			return _regions.Any(x => x.Key >= from && x.Key < to && x.Value.Equals(value));
+			List<KeyValuePair<uint, T>> relevantMarkers = _regions.Where(x => x.Key >= from && x.Key < to).ToList();
+
+			if(from > 0 && (relevantMarkers.Count == 0 || relevantMarkers[0].Key != from))
+			{
+				relevantMarkers.Add(_regions.Last(x => x.Key < from));
+			}
+
+			return relevantMarkers.Any(x => x.Value.Equals(value));
 		}
 	}
 }
