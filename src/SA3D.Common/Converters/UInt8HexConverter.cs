c90ab75b912e05a8477b2367bd11f34e5d755d45
Justin113D <Justin113D@gmail.com>
2026-05-18 11:09:20 +0200

    Added byte-hex-converter

---
+++ src/SA3D.Common/Converters/UInt8HexConverter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace SA3D.Common.Converters
+{
+	/// <summary>
+	/// A valueconverter for unsigned hexadecimal 16 bit numbers
+	/// </summary>
+	public class UInt8HexConverter : TypeConverter
+	{
+		/// <inheritdoc/>
+		public override bool CanConvertTo(ITypeDescriptorContext? context, Type? destinationType)
+		{
+			if(destinationType == typeof(string))
+			{
+				return true;
+			}
+
+			return base.CanConvertTo(context, destinationType);
+		}
+
+		/// <inheritdoc/>
+		public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
+		{
+			if(destinationType == typeof(string) && value is byte integer)
+			{
+				return ConvertTo(integer);
+			}
+
+			return base.ConvertTo(context, culture, value, destinationType);
+		}
+
+		/// <summary>
+		/// Converts an unsigned 8-bit integer to a hexadecimal string value
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static string ConvertTo(byte value)
+		{
+			return value.ToString("X");
+		}
+
+		/// <inheritdoc/>
+		public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+		{
+			if(sourceType == typeof(string))
+			{
+				return true;
+			}
+
+			return base.CanConvertFrom(context, sourceType);
+		}
+
+		/// <inheritdoc/>
+		public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+		{
+			if(value is string str)
+			{
+				return ConvertFrom(str, context?.PropertyDescriptor?.Name);
+			}
+
+			return base.ConvertFrom(context, culture, value);
+		}
+
+		/// <summary>
+		/// Converts a hexadecimal string value to an unsigned 8-bit integer
+		/// </summary>
+		/// <param name="value">The value to convert</param>
+		/// <param name="debugName">Name by which to identify the value being converted</param>
+		/// <returns></returns>
+		public static byte ConvertFrom(string value, string? debugName = null)
+		{
+			if(byte.TryParse(value, NumberStyles.HexNumber, null, out byte result))
+			{
+				return result;
+			}
+
+			throw new InvalidCastException($"Failed to cast {(string.IsNullOrWhiteSpace(debugName) ? "?" : debugName)} from hex to ushort! Value: {value}");
+		}
+
+		/// <inheritdoc/>
+		public override bool IsValid(ITypeDescriptorContext? context, object? value)
+		{
+			if(value is ushort)
+			{
+				return true;
+			}
+
+			if(value is string str)
+			{
+				return ushort.TryParse(str, NumberStyles.HexNumber, NumberFormatInfo.InvariantInfo, out _);
+			}
+
+			return base.IsValid(context, value);
+		}
+	}
+}
