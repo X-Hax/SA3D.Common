bef0acca597adda1a07154324ef704f33545491f
Justin113D <Justin113D@gmail.com>
2026-06-11 18:15:18 +0200

    Moved file interface from Modeling to here

---
+++ src/SA3D.Common/IO/IFileSerializable.cs
@@ -0,0 +1,22 @@
+﻿using Amicitia.IO.Binary;
+
+namespace SA3D.Common.IO
+{
+	/// <summary>
+	/// Base file interface
+	/// </summary>
+	public interface IFileSerializable : IBinarySerializable
+	{
+		/// <summary>
+		/// Check whether the data behind a reader can be read as the file
+		/// </summary>
+		/// <param name="reader"></param>
+		/// <returns></returns>
+		public bool Check(BinaryObjectReader reader);
+	}
+
+	/// <summary>
+	/// Base file interface (with a context)
+	/// </summary>
+	public interface IFileSerializable<T> : IBinarySerializable<T>, IFileSerializable { }
+}
